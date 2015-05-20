06c52b0ed6dc9fd89d28b5b035e3c2279c9058f9
Riaan <riaan@Meran-iMac.local>
2015-05-20 18:49:55 +0200

    Initial Commit

---
+++ CoreDataExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataExample
+//
+//  Created by Riaan on 2015/05/20.
+//  Copyright (c) 2015 Riaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CoreDataExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreDataExample
+//
+//  Created by Riaan on 2015/05/20.
+//  Copyright (c) 2015 Riaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
